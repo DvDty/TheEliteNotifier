46b214cbbed1700505c93d6eb4921a8978858549
Pavel <dvdtygc@gmail.com>
2017-12-22 18:31:50 +0200

    Data pull update

--- core/getData.php
+++ core/getData.php
@@ -1,7 +1,18 @@
 <?php
 
-function getData() {
+function getData()
+{
     $rssUrl = "https://rankings.the-elite.net/ge-untieds.rss";
+    $rssData = file_get_contents($rssUrl);
 
+    $rssData = explode(
+        '<webMaster>ryandwyer1@gmail.com (Ryan Dwyer)</webMaster>',
+        $rssData
+    );
+
+    $rssData = explode('</channel>', $rssData[1]);
+    var_dump($rssData);
     //TODO: rss to json
 }
+
+getData();
