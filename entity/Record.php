1ef7edc4e62f1d4aa01b342bc796f7d6f101cff5
Pavel <dvdtygc@gmail.com>
2017-12-22 19:35:31 +0200

    Add record entity

---
+++ entity/Record.php
@@ -0,0 +1,84 @@
+<?php
+
+class Record
+{
+    private $title;
+    private $description;
+    private $date;
+    private $url;
+
+    public function __construct(string $title = '',
+                                string $description = '',
+                                DateTime $date,
+                                string $url = '')
+    {
+        $this->title = $title;
+        $this->description = $description;
+        $this->date = $date;
+        $this->url = $url;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * @param mixed $date
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * @param mixed $url
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+    }
+}
