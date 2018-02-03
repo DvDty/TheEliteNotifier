3a7938ea749f26e217e888a83d6e0fc29eaa60e2
Pavel <dvdtygc@gmail.com>
2018-02-03 19:22:10 +0200

    Add support for multiple emails

--- core/notification.php
+++ core/notification.php
@@ -5,19 +5,24 @@
  */
 function notification(array $records): void
 {
-    $to = 'dvdtygc@gmail.com';
-    $from = 'golden-eye@aluminabuild.com';
+	$TOs = [
+		'dvdtygc@gmail.com',
+	];
 
-    $headers = 'MIME-Version: 1.0' . "\r\n";
-    $headers .= 'From: ' . $from . "\r\n";
-    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+	$from = 'golden-eye@aluminabuild.com';
 
-    $subject = 'Untied world records update!';
-    $message = '<h1>The following updates have been made in the elite rank list:</h1><br><br>';
+	$headers = 'MIME-Version: 1.0' . "\r\n";
+	$headers .= 'From: ' . $from . "\r\n";
+	$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 
-    foreach ($records as $record) {
-        $message .= $record->getTitle() . "<br>";
-    }
+	$subject = 'Untied world records update!';
+	$message = '<h1>The following updates have been made in the elite rank list:</h1><br><br>';
 
-    mail($to, $subject, $message, $headers);
+	foreach ($records as $record) {
+		$message .= $record->getTitle() . "<br>";
+	}
+
+	foreach ($TOs as $to) {
+		mail($to, $subject, $message, $headers);
+	}
 }
