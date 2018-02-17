b62db361710ddc20262e3b5e10385157aee94c0b
Pavel <dvdtygc@gmail.com>
2018-02-18 01:56:02 +0200

    Old logic is moved, models are no longer needed

--- core/core.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include 'notification.php';
-include 'getRss.php';
-include 'rssToJson.php';
-include 'rssToObjectsArray.php';
-include 'jsonToObjectsArray.php';
-include 'save.php';
-include 'check.php';
--- core/save.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function save(string $records): void
-{
-	$file = fopen('data/records.json', 'w+');
-
-	fwrite($file, $records);
-	fclose($file);
-}
