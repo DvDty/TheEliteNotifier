9e256a51ef5abdb8e5db2c72aef5b7a3c3a3d672
Pavel Grancharov <dvdtygc@gmail.com>
2018-10-30 14:41:03 +0200

    Merge updating functionality into records controller

--- src/controllers/RecordsController.php
+++ src/controllers/RecordsController.php
@@ -4,27 +4,62 @@ namespace DvDty\TheEliteNotifier\Controllers;
 
 final class RecordsController extends Controller
 {
+	private $liveRecords = [];
+	private $savedRecords = [];
+	private $newRecords = [];
 
-	public  function getRecords(): array
+	private $types = [
+		'ge-untieds',
+		//		'ge-wrs',
+		//		'pd-untieds',
+		//		'pd-wrs',
+	];
+
+	const RSS_LIMIT = 50;
+	const API_BASE_URL = 'https://rankings.the-elite.net/';
+
+
+	public function execute()
+	{
+		$this->getNewRecords();
+		$this->getOldRecords();
+
+		$this->compareRecords();
+
+		if ($this->newRecords) {
+			$this->saveNewRecords();
+			$this->notifyUsers();
+		}
+	}
+
+
+	private function getNewRecords(): void
 	{
 		$records = [];
 
-		$urls = [
-			'ge-uwr' => 'https://rankings.the-elite.net/ge-untieds.rss',
-			'ge-wr'  => 'https://rankings.the-elite.net/ge-wrs.rss',
-			'pd-uwr' => 'https://rankings.the-elite.net/pd-untieds.rss',
-			'pd-wr'  => 'https://rankings.the-elite.net/pd-wrs.rss'
-		];
-
-		foreach ($urls as $type => $url) {
-			$records[$type] = $this->fetchRss($url);
+		foreach ($this->types as $type) {
+			$records[$type] = $this->fetchRss($type);
 		}
 
-		return $records;
+		$this->liveRecords = $records;
 	}
 
-	public function fetchRss($url = ''): array {
+
+	private function getOldRecords(): void
+	{
+		foreach ($this->types as $type) {
+			$records = file_get_contents('src/resources/records/' . $type . '.json');
+			$this->savedRecords[$type] = json_decode($records);
+		}
+	}
+
+
+	private function fetchRss(string $type = 'ge-untieds'): array
+	{
+		$url = $this->getApiUrl($type);
 		$rss = file_get_contents($url);
+
+		$rss = str_replace('rnk:', '', $rss);
 
 		if (!$rss) {
 			$this->email->sendException('url is down');
@@ -34,5 +69,38 @@ final class RecordsController extends Controller
 		$std = simplexml_load_string($rss)->channel;
 
 		return json_decode(json_encode($std))->item;
+	}
+
+
+	private function getApiUrl(string $type = 'ge-untieds', string $extension = 'rss')
+	{
+		return self::API_BASE_URL . $type . '.' . $extension;
+	}
+
+
+	private function compareRecords(): void
+	{
+		foreach ($this->types as $type) {
+			for ($i = 0; $i < self::RSS_LIMIT; $i++) {
+				if ($this->savedRecords[$type][0]->title != $this->liveRecords[$type][$i]->title) {
+					$this->newRecords[] = $this->liveRecords['ge-untieds'][$i];
+					continue;
+				}
+
+				break;
+			}
+		}
+	}
+
+
+	private function saveNewRecords()
+	{
+
+	}
+
+
+	private function notifyUsers()
+	{
+
 	}
 }
